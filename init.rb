a00ab99e7d45f0fe0a94c9a7554533d94494fdcd
Syntactic Vexation <syntacticvexation@gmail.com>
2011-06-07 17:20:49 +1000

    closes bug #83 favourite projects now has a visible tab in My Account properties

--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ Redmine::Plugin.register :redmine_favourite_projects do
   name 'Redmine Favourite Projects plugin'
   author 'Syntactic Vexation'
   description 'This is a plugin for Redmine to provide a list of favourite projects on My Page'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/syntacticvexation/redmine_favourite_projects'
 end
 
 # initialize hook
-class GitosisPublicKeyHook < Redmine::Hook::ViewListener
+class FavouritesEditHook < Redmine::Hook::ViewListener
   render_on :view_my_account_contextual, :inline => "| <%= link_to(l('favourite_projects_box'), { :controller => 'favourite_projects', :action => 'index' }) %>"
 end
