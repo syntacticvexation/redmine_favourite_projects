6b06ea88aa9d5d1e2e80c02f01ace53cb8f78209
jamesbondos <bondos@gmail.com>
2011-10-12 15:12:01 +0200

    fix for error with no method to_s on refreshes

--- app/models/favourite_project.rb
+++ app/models/favourite_project.rb
@@ -1,4 +1,5 @@
 class FavouriteProject < ActiveRecord::Base
+  unloadable
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   belongs_to :project, :class_name => "Project", :foreign_key => "project_id"
 end
