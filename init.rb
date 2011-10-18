124a131fc884226e724e43cdd1a306681aa555bb
jamesbondos <bondos@gmail.com>
2011-10-18 17:00:26 +0200

    Render favourite projects on top menu

--- init.rb
+++ init.rb
@@ -6,9 +6,18 @@ Redmine::Plugin.register :redmine_favourite_projects do
   description 'This is a plugin for Redmine to provide a list of favourite projects on My Page'
   version '0.0.2'
   url 'https://github.com/syntacticvexation/redmine_favourite_projects'
+  
+  menu :top_menu, :favourite_menu, '',
+  {
+    :caption => '',
+    :html => { :id => 'favourite-menu' },
+    :param => :project_id,
+    :if => Proc.new { User.current.logged? }
+  }
 end
 
 # initialize hook
 class FavouritesEditHook < Redmine::Hook::ViewListener
+  render_on :view_layouts_base_body_bottom, :partial => 'favourite_projects/update_menu'
   render_on :view_my_account_contextual, :inline => "| <%= link_to(l('favourite_projects_box'), { :controller => 'favourite_projects', :action => 'index' }) %>"
 end
