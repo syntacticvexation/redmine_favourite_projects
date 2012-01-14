ab2b0c8afa5da1c0f65b432bded959855fc50b63
Syntactic Vexation <syntacticvexation@gmail.com>
2012-01-15 02:55:26 +1000

    refs github #11 - as default routes are no longer supported by Redmine > 1.4, added default route for plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :favourite_projects, :controller => 'favourite_projects', :path_prefix => 'my'
+    map.connect 'favourite_projects/:action', :controller => 'favourite_projects'
 end
