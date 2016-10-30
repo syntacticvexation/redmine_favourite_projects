abf934f67933b1b90eb718dba917ee4921b9e9bc
Syntactic Vexation <syntacticvexation@gmail.com>
2016-10-31 09:01:55 +1000

    fixes #55 - Links are not displayed in the topbar, but in bottom of each page

--- init.rb
+++ init.rb
@@ -23,6 +23,10 @@ if Rails::VERSION::MAJOR >= 3
     unless MyHelper.included_modules.include?(FavouriteProjectsMyHelperPatch)
       MyHelper.send(:include, FavouriteProjectsMyHelperPatch)
     end
+
+    unless Redmine::MenuManager::MenuHelper.included_modules.include?(FavouriteProjectsMenuPatch)
+      Redmine::MenuManager::MenuHelper.send(:include, FavouriteProjectsMenuPatch)
+    end
   end
 else
   Dispatcher.to_prepare :redmine_favourite_projects do
@@ -43,6 +47,10 @@ else
     unless MyHelper.included_modules.include?(FavouriteProjectsMyHelperPatch)
       MyHelper.send(:include, FavouriteProjectsMyHelperPatch)
     end
+
+    unless Redmine::MenuManager::MenuHelper.included_modules.include?(FavouriteProjectsMenuPatch)
+      Redmine::MenuManager::MenuHelper.send(:include, FavouriteProjectsMenuPatch)
+    end
   end
 end
 
@@ -50,7 +58,7 @@ Redmine::Plugin.register :redmine_favourite_projects do
   name 'Redmine Favourite Projects plugin'
   author 'Syntactic Vexation'
   description 'This is a plugin for Redmine to provide a list of favourite projects on My Page, Top Menu or Project Jumplist'
-  version '0.8.2'
+  version '0.9'
   url 'https://github.com/syntacticvexation/redmine_favourite_projects'
 
   
@@ -61,18 +69,9 @@ Redmine::Plugin.register :redmine_favourite_projects do
     'allowUserOverride' => true
     },
     :partial => 'redmine_favourite_projects'
-  
-  menu :top_menu, :favourite_menu, '',
-  {
-    :caption => '',
-    :html => { :id => 'favourite-menu' },
-    :param => :project_id,
-    :if => Proc.new { User.current.logged? and Object.new.extend(FavouriteProjectsApplicationHelperPatch::InstanceMethods).calculated_favourite_projects_settings['modifyTopMenu'] }
-  }
 end
 
 # initialize hook
 class FavouritesEditHook < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_body_bottom, :partial => 'favourite_projects/update_menu'
   render_on :view_my_account_contextual, :inline => "| <%= link_to(l('favourite_projects_box'), { :controller => 'favourite_projects', :action => 'index' }) %>"
 end
---
+++ lib/favourite_projects_menu_patch.rb
@@ -0,0 +1,32 @@
+module FavouriteProjectsMenuPatch
+
+  def self.included(base)
+    base.send(:extend, ClassMethods)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :render_menu, :favourite_projects
+    end
+  end
+
+  module ClassMethods
+  end
+
+  module InstanceMethods
+    def render_menu_with_favourite_projects(menu, project=nil)
+      if (menu == :top_menu and calculated_favourite_projects_settings['modifyTopMenu'])
+          links = []
+          menu_items_for(menu, project) do |node|
+            links << render_menu_node(node, project)
+          end
+
+          User.current.ordered_favourite_projects.each do |fav|
+            links << link_to(fav.project.name, fav.project, :class => 'icon icon-projects')
+          end
+
+          links.empty? ? nil : content_tag('ul', links.join.html_safe)
+      else
+        render_menu_without_favourite_projects(menu, project)
+      end
+    end
+  end
+end
